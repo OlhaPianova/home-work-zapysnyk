7d3bbccc4df67bca60c0d6ba968bd66b59ae9dbf
olga <piolmaolga@gmail.com>
2020-03-06 12:06:13 +0200

    first commit2

---
+++ Zapysnik/Zapysnyk.cpp
@@ -0,0 +1,6 @@
+#include "Zapysnyk.h"
+
+void Add_Podiya(){
+	Zapysnyk newPodiya;
+	cout << "Saholovok for new podiya : " << endl;
+}
---
+++ Zapysnik/Zapysnyk.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+struct Zapysnyk {
+	string sagolovok;
+	string opys;
+	string date;
+	string time;
+	int level;
+	string katehoria;
+};
+
+void Add_Podiya();
